d23853b7ab4c95d8e8ded52d965dc0b6a218dc6b
lukaofirst <lucasmacedo123@gmail.com>
2021-11-09 00:45:06 -0300

    creating cards, installing FontAwesome and useState() hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,181 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { useState, Fragment } from 'react';
+// import { Modal, Button } from 'react-bootstrap';
 import './App.css';
 
-function App() {
-    const [show, setShow] = useState(false);
+let initialState = [
+    {
+        id: 1,
+        prioridade: 3,
+        titulo: 'título',
+        descricao: 'Primeira Atividade',
+    },
+    {
+        id: 2,
+        prioridade: 2,
+        titulo: 'título',
+        descricao: 'Segunda Atividade',
+    },
+];
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+const App = () => {
+    const [atividades, setAtividades] = useState(initialState);
+
+    const prioriedadeLabel = (arg) => {
+        switch (arg) {
+            case 1:
+                return 'Baixa';
+            case 2:
+                return 'Normal';
+            case 3:
+                return 'Alta';
+            default:
+                return 'Não definido';
+        }
+    };
+
+    const prioriedadeStyle = (arg) => {
+        switch (arg) {
+            case 1:
+                return {
+                    icon: 'smile',
+                    color: 'dark',
+                };
+            case 2:
+                return {
+                    icon: 'meh',
+                    color: 'success',
+                };
+            case 3:
+                return {
+                    icon: 'frown',
+                    color: 'warning',
+                };
+            default:
+                return 'Não definido';
+        }
+    };
+
+    const addAtividade = (e) => {
+        e.preventDefault();
+
+        const atividade = {
+            id: document.getElementById('id').value,
+            prioridade: document.getElementById('prioridade').value,
+            titulo: document.getElementById('titulo').value,
+            descricao: document.getElementById('descricao').value,
+        };
+
+        setAtividades([...atividades, { ...atividade }]);
+    };
 
     return (
-        <div className='p-2'>
-            <Button variant='primary' onClick={handleShow}>
-                Launch demo modal
-            </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Woohoo, you're reading this text in a modal!
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant='secondary' onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant='primary' onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
+        <Fragment>
+            <div className='container p-2'>
+                <form className='row g-3'>
+                    <div className='col-md-6'>
+                        <label className='form-label'>Id</label>
+                        <input
+                            id='id'
+                            type='text'
+                            className='form-control'
+                            readOnly
+                            value={
+                                Math.max.apply(
+                                    Math,
+                                    atividades.map((item) => item.id)
+                                ) + 1
+                            }
+                        />
+                    </div>
+                    <div className='col-md-6'>
+                        <label className='form-label'>Prioridade</label>
+                        <select className='form-select' id='prioridade'>
+                            <option defaultValue=''>Selecione...</option>
+                            <option value='1'>Baixa</option>
+                            <option value='2'>Normal</option>
+                            <option value='3'>Alta</option>
+                        </select>
+                    </div>
+                    <div className='col-md-6'>
+                        <label className='form-label'>Título</label>
+                        <input
+                            id='titulo'
+                            type='text'
+                            className='form-control'
+                        />
+                    </div>
+                    <div className='col-md-6'>
+                        <label className='form-label'>Descrição</label>
+                        <input
+                            id='descricao'
+                            type='text'
+                            className='form-control'
+                        />
+                    </div>
+                    <hr />
+                    <div className='col-12'>
+                        <button
+                            className='btn btn-outline-secondary'
+                            onClick={addAtividade}
+                        >
+                            + Atividade
+                        </button>
+                    </div>
+                </form>
+                <div className='mt-3'>
+                    {atividades.map((ativ) => (
+                        <div
+                            key={ativ.id}
+                            className={`card mb-2 shadow-sm border border-${
+                                prioriedadeStyle(ativ.prioridade).color
+                            }`}
+                        >
+                            <div className='card-body'>
+                                <div className='d-flex justify-content-between'>
+                                    <h5 className='card-title'>
+                                        <span className='badge bg-secondary me-1'>
+                                            {ativ.id}
+                                        </span>
+                                        - {ativ.titulo}
+                                    </h5>
+                                    <h6>
+                                        Prioridade:
+                                        <span
+                                            className={`ms-1 text-${
+                                                prioriedadeStyle(
+                                                    ativ.prioridade
+                                                ).color
+                                            }`}
+                                        >
+                                            <i
+                                                className={`me-1 far fa-${
+                                                    prioriedadeStyle(
+                                                        ativ.prioridade
+                                                    ).icon
+                                                }`}
+                                            ></i>
+                                            {prioriedadeLabel(ativ.prioridade)}
+                                        </span>
+                                    </h6>
+                                </div>
+                                <p className='card-text'>{ativ.descricao}</p>
+                                <div className='d-flex justify-content-end border-top pt-2 m-0'>
+                                    <button className='btn btn-sm btn-outline-primary me-2'>
+                                        <i className='fas fa-pen me-2'></i>
+                                        Editar
+                                    </button>
+                                    <button className='btn btn-sm btn-outline-danger'>
+                                        <i className='fas fa-trash me-2'></i>
+                                        Excluir
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </Fragment>
     );
-}
+};
 
 export default App;
